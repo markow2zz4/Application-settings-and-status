c87c6ad3bcef934f34ca74a3ad9679b628285290
MarkoW <shpetai@mer.ci.nsu.ru>
2023-04-10 16:06:30 +0700

    create a save instance state in Application

--- app/src/main/java/com/example/saveinstancestate/MainActivity.java
+++ app/src/main/java/com/example/saveinstancestate/MainActivity.java
@@ -3,12 +3,48 @@ package com.example.saveinstancestate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    String name ="не определен";
+    final static String nameVariableKey = "NAME_VAR";
+    final static String textViewTexKey = "TEXT_VIEW";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void restoreField(View view)
+    {
+        TextView nameView = (TextView) findViewById(R.id.saveTextView);
+        nameView.setText(name);
+    }
+
+    public void saveField(View view)
+    {
+        TextView nameBox = (TextView) findViewById(R.id.editTextTextMultiLine);
+        name = nameBox.getText().toString();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState)
+    {
+        outState.putString(nameVariableKey, name);
+        TextView nameView = (TextView) findViewById(R.id.saveTextView);
+        outState.putString(textViewTexKey, nameView.getText().toString());
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState)
+    {
+        super.onRestoreInstanceState(savedInstanceState);
+        name = savedInstanceState.getString(nameVariableKey);
+        String textViewText= savedInstanceState.getString(textViewTexKey);
+        TextView nameView = (TextView) findViewById(R.id.saveTextView);
+        nameView.setText(textViewText);
     }
 }
